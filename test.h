f8fafe4b7c72cb7ba14285b643bfa86168d18799
joshsyp <doublezone4.0@gmail.com>
2019-08-29 17:37:28 -0400

    update

--- test.h
+++ test.h
@@ -12,6 +12,7 @@ int sidesCount;
 public:
 
 shape(): sides(NULL), sidesCount(0) {}
+
 shape (const shape& obj) //copy constructor
 {
     if (obj.sides == NULL)
@@ -21,9 +22,9 @@ shape (const shape& obj) //copy constructor
     }
 else
     {
-    sidesCount=obj.sidesCount;
+    sidesCount = obj.sidesCount;
     sides = new double[sidesCount]; //sides is a dynamic
-    for(int i=0;i<sidesCount;i+=1)
+    for(int i=0;i< sidesCount;i+=1)
     {
         sides[i]=obj.sides[i];
     }
@@ -64,3 +65,183 @@ virtual double Area() const =0;
 virtual double Area() =0;
 }
 #endif
+
+
+//
+//
+//
+class shape
+{
+    public:
+    shape(int size): sidesCount(size)
+{
+    if (sidesCount<=0) 
+{
+    sidesCount=3;
+}
+    sides=new double [sidesCount];
+    for (inti=0;i<sidesCount;i+=1)
+{
+    sides[i]=1.0;
+}
+}
+//Encapsulation
+
+int Getsides() const
+{
+    return sidesCount;
+}
+
+int Getsides()
+{
+    return sidesCount;
+}
+
+double Getsides (int index) const
+
+{
+    if(index>=0 && index < sidesCount)
+    {
+        return sides[index;]
+    }
+throw "out of bound";
+}
+
+
+//
+
+int Getsides() const
+{
+    return sidesCount;
+}
+
+int Getsides()
+{
+    return sidesCount;
+}
+
+double Getsides (int index) const
+
+{
+    if(index>=0 && index < sidesCount)
+{
+        return sides[index;]
+}
+throw "out of bound";
+}
+
+
+void SetSide(int index, double value)
+{
+    if (index >=0 && index < sidesCount && value > 0)
+{
+    sides[index]=value;
+}
+}
+
+const double& operator[](int index) const
+{
+    if(index > = 0 && index < sidesCount)
+    {
+        return sides[index];
+    }
+    throw "out of bound";
+    }
+    
+double & opreator [](index)
+
+{
+if(index > = 0 && index < sidesCount)
+    {
+        return sides[index];
+    }
+    throw "out of bound";
+}
+
+// Inheritance
+
+class Rectangle: public shape
+{
+    public:
+    rectangle(): shape(2) {}
+    Rectangle(double lenght,double width):
+    Rectangle()
+{
+    if(length > 0 && width > 0)
+    {
+        SetSide(0,(length < width)?(length): (width));
+        SetSide(1,(length > width)? (lenght): (width));
+    }
+
+}
+}
+
+Rectangle (const Rectangle & obj):
+shape(dynamic_cast < const shape& > (obj)) {}
+
+Rectangle & operator =(const rectangle & rhs)
+{
+    if(this !=&rhs)
+    {
+        shape::operator=(dynamci_cast <const shape & >(rhs));
+    }
+    return *this;
+}
+
+
+//
+~Rectangle () {}
+
+void setWidth(double value)
+{
+    if(Getsides(1) > value && value > 0)
+    {
+        SetSide(0,value);
+    }
+}
+
+void SetLength (double value)
+{
+    if (Getsides(0) < value && value > 0)
+    {
+        SetSide(1,value);
+    }
+}
+
+double GetWidth()
+{
+    return Getsides(0);
+}
+double GetLength()
+{
+    return Getsides(1);
+}
+}
+
+double perimeter() const
+{
+    return 2*(Getsides(0)+Getsides(1));
+
+}
+double perimeter()
+{
+    return 2*(Getsides(0)+Getsides(1));
+
+}
+
+double Area() const
+
+{
+    return Getsides(0)*Getsides(1);
+}
+
+double Area()
+{
+    return Getsides(0)*Getsides(1);
+}
+
+private:
+    using   shape::Getside;
+    using   shape::Setsides;
+    using   shape::Getsides;
+    using   shape::Getsides const;
